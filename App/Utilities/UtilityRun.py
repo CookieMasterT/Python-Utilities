6c4e8d54f8be35ea91272be9121829392769d12b
Tytus <tytus@baranscy.info>
2025-12-11 17:21:12 +0100

    Expanded UtilityRun.py to be able to manage running scripts

--- App/Utilities/UtilityRun.py
+++ App/Utilities/UtilityRun.py
@@ -1,26 +1,51 @@
 import asyncio
 import os
 import re
-
-
 from App.internal_scripts import ConfigREST
 
+processes = {}
 
-async def run_scripts():
-    config = ConfigREST.get("*")
-    expression = re.compile(r"^Utilities\.(?P<UtilityName>.*)\.Enabled$")
-    enabled_utilities = []
-    for option in config:
-        match = re.fullmatch(expression, option)
-        if match and config[option] is True:
-            enabled_utilities.append(match.group('UtilityName'))
-    processes = []
-    for item in os.listdir():
-        if item in enabled_utilities:
-            processes.append(await asyncio.create_subprocess_exec("python", f"./{item}/{item}.pyw"))
-    for proc in processes:
-        await proc.wait()  # wait for processes to finish
+
+def get_all_scripts(ignore_config=False):
+    scripts = []
+    if ignore_config:
+        for item in os.listdir():
+            scripts.append(item)
+    else:
+        config = ConfigREST.get("*")
+        expression = re.compile(r"^Utilities\.(?P<UtilityName>.*)\.Enabled$")
+        enabled_utilities = []
+        for option in config:
+            match = re.fullmatch(expression, option)
+            if match and config[option] is True:
+                enabled_utilities.append(match.group('UtilityName'))
+        for item in os.listdir():
+            if ignore_config or item in enabled_utilities:
+                scripts.append(item)
+    return scripts
+
+
+async def run_all_scripts_async():
+    for item in get_all_scripts():
+        await run_script_async(item)
+
+
+async def run_script_async(item: str):
+    proc = await asyncio.create_subprocess_exec("python", f"./{item}/{item}.pyw")
+    processes[item] = proc
+    await proc.wait()
+
+
+def stop_all_scripts():
+    for item in processes:
+        stop_script(item)
+
+
+def stop_script(item: str):
+    proc = processes[item]
+    if proc.returncode:
+        proc.terminate()
 
 
 if __name__ == "__main__":
-    asyncio.run(run_scripts())
+    asyncio.run(run_all_scripts_async())
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Python-Utilities
-A python repository to store and manage python utilities for Windows QoL, think of it as powertoys.
+A python repository to store and manage python utilities for Windows QoL, think of it as PowerToys.
 
-I will put the scripts I make here
+The project needs the global_hotkeys and pillow libraries to be installed in order to be able to run.
 
-Currently they are to be run by adding a shortcut to App\Utilities\UtilityRun.py in windows autostart
+Currently, they are to be run by adding a shortcut to App\Utilities\UtilityRun.py in windows autostart.
 
  - C:\Users\\%USER%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 
- - or Run - "shell:startup" (windows + R)
+ - or Run - "shell:startup" (windows + R).
